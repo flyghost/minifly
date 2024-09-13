643b71f16e02dd3cbdbe6af66ad2c12303e227e8
creekwater <zhaoyang_wu@foxmail.com>
2024-09-14 00:52:59 +0800

    同步IAP头文件

--- boot/robot/IAP/iap.h
+++ boot/robot/IAP/iap.h
@@ -2,23 +2,13 @@
 #define __IAP_H
 #include "sys.h"  
 
-/********************************************************************************	 
- * 本程序只供学习使用，未经作者许可，不得用于其它任何用途
- * ALIENTEK MiniFly
- * IAP代码	
- * 正点原子@ALIENTEK
- * 技术论坛:www.openedv.com
- * 创建日期:2017/5/2
- * 版本：V1.0
- * 版权所有，盗版必究。
- * Copyright(C) 广州市星翼电子科技有限公司 2014-2024
- * All rights reserved
-********************************************************************************/  
 
 
 typedef  void (*iapfun)(void);				//定义一个函数类型的参数.   
-	   
-void iap_load_app(u32 appxaddr);			//跳转到APP程序执行
+
+// #define FLASH_APP1_ADDR		0x08010000  	//第一个应用程序起始地址(存放在FLASH)
+											//保留0X08000000~0X0800FFFF的空间为IAP使用
+void iap_load_app(u32 appxaddr);			//跳转到APP程序执行，执行flash里面的app程序
 void iap_write_appbin(u32 appxaddr,u8 *appbuf,u32 applen);	//在指定地址开始,写入bin
 
 #endif
